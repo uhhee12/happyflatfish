a88e0cd1e77661937e3db089f53c064af293b748
uhhee12 <yunhee4560@gmail.com>
2023-01-31 23:57:08 +0900

    스크립트 수정

--- js/main.js
+++ js/main.js
@@ -1,45 +1,9 @@
-$(document).ready(function () {
-    $('#chk-toggle').click(function () {
-        if ($(this).is(':checked') == true) {
-            $('.black').removeClass('show');
-        } else {
-            $('.black').addClass('show');
-        }
-    });
-
-    $('.call').click(function () {
-        $(this)
-            .parent()
-            .find('.sub1-subUl')
-            .slideToggle();
-        $(this)
-            .children('.list3_img1')
-            .toggleClass('rotate');
-    });
-
-    $('.liTitleBox').click(function () {
-        $(this)
-            .parent()
-            .find('.hide')
-            .slideToggle();
-        $(this)
-            .children('img')
-            .toggleClass('rotate');
-    });
-
-    $('.suOrange').click(function () {
-        $('.setpop').fadeIn();
-    });
-
-    $('.pop2Btn1').click(function () {
-        $('.setpop').fadeOut();
-    });
-
-    $('#chk-toggle').click(function () {
-        if ($(this).is(':checked') == true) {
-            $('.black').removeClass('show');
-        } else {
-            $('.black').addClass('show');
-        }
-    });
-});
+ $(document).ready(function () {
+     $('#chk-toggle').click(function () {
+         if ($(this).is(':checked') == true) {
+             $('.black').removeClass('show');
+         } else {
+             $('.black').addClass('show');
+         }
+     });
+ });

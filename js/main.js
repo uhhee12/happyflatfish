f5f68b153bca5c79e7a50e3100520471ec0b3d3f
uhhee12 <yunhee4560@gmail.com>
2023-02-01 00:03:32 +0900

    스크립트 수정

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
- $(document).ready(function () {
+$(document).ready(function () {
+      $('.call').click(function() {
+                $(this).parent().find('.sub1-subUl').slideToggle();
+                $(this).children('.list3_img1').toggleClass('rotate');
+            });
+
+            $('.liTitleBox').click(function() {
+                $(this).parent().find('.hide').slideToggle();
+                $(this).children('img').toggleClass('rotate');
+            });
+
+            $('.suOrange').click(function() {
+                $('.setpop').fadeIn();
+            });
+
+            $('.pop2Btn1').click(function() {
+                $('.setpop').fadeOut();
+            });
+            
+            $('#chk-toggle').click(function(){
+               if($(this).is(':checked') == true){
+                   $('.black').removeClass('show');
+               }else{
+                   $('.black').addClass('show');
+               }
+            });
      $('#chk-toggle').click(function () {
          if ($(this).is(':checked') == true) {
              $('.black').removeClass('show');
